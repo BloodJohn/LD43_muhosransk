c79d5861e2b57cf6f809b5c73966e2fcba1be597
Eugene Pavlov <bjohn.ext@gmail.com>
2018-12-02 11:43:07 +0300

    upd MissionController

--- Assets/Script/CheckPoint.cs
+++ Assets/Script/CheckPoint.cs
@@ -3,34 +3,77 @@ using VehicleBehaviour;
 
 public class CheckPoint : MonoBehaviour
 {
-    public string StartMission = "New Mission";
-    public string CompleteMission = "Complete Mission";
+    public string StartMessage = "New Mission";
+    public string AlertMessage = "Please Help";
+    public string CompleteMessage = "Complete Mission";
+    public string FailMessage = "Mission Failed";
 
     private WheelVehicle _playerCar;
-    public bool IsComplete;
+    private bool _isAlert;
+    private bool _isFail;
+    private bool _isComplete;
+    private float _time;
+
+
     private const float _radius2Check = 2f*2f;
+    private const float _alertDelay = 5f;
+    private const float _failDelay = 15f;
 
     void Awake()
     {
         _playerCar = FindObjectOfType<WheelVehicle>();
     }
 
-    private void Start()
+    public void StartMission()
     {
-        MissionUI.Instance.SendMessage(StartMission);
+        if (IsNearPoint) return;
+        if (_isFail) return;
+
+        _isComplete = false;
+        _isFail = false;
+        _time = 0f;
+        gameObject.SetActive(true);
+        MissionUI.Instance.SendMessage(StartMessage);
     }
 
     public void CheckComplete()
     {
-        if (IsComplete) return;
+        if (!gameObject.activeSelf) return;
+        if (_isFail) return;
+        if (_isComplete) return;
+        _time += Time.deltaTime;
 
-        var dist2 = (transform.position - _playerCar.transform.position).sqrMagnitude;
-
-        if (dist2 <= _radius2Check)
+        if (IsNearPoint)
         {
-            MissionUI.Instance.SendMessage(CompleteMission);
-            IsComplete = true;
+            _isComplete = true;
             gameObject.SetActive(false);
+            MissionUI.Instance.SendMessage(CompleteMessage);
+            return;
+        }
+
+        if (!_isAlert && _time > _alertDelay)
+        {
+            _isAlert = true;
+            MissionUI.Instance.SendMessage(AlertMessage);
+            return;
+        }
+
+        if (_time > _failDelay)
+        {
+            _isFail = true;
+            gameObject.SetActive(false);
+            MissionUI.Instance.SendMessage(FailMessage);
+            return;
+        }
+    }
+
+    private bool IsNearPoint
+    {
+        get
+        {
+            var dist2 = (transform.position - _playerCar.transform.position).sqrMagnitude;
+
+            return (dist2 <= _radius2Check);
         }
     }
 }
--- Assets/Script/MissionController.cs
+++ Assets/Script/MissionController.cs
@@ -1,25 +1,43 @@
 ﻿using System;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MissionController : MonoBehaviour
 {
     public static MissionController Instance;
     private CheckPoint[] _missionList;
 
+    public CheckPoint CurrentMission => _missionList.FirstOrDefault(item => item.gameObject.activeSelf);
+
     void Awake()
     {
         Instance = this;
         _missionList = FindObjectsOfType<CheckPoint>();
+
+        foreach (var checkPoint in _missionList)
+        {
+            checkPoint.gameObject.SetActive(false);
+        }
+
+        Random.InitState(DateTime.Now.Millisecond);
     }
 
     private void Update()
     {
+        if (CurrentMission == null)
+        {
+            var index = Random.Range(0, _missionList.Length);
+
+            _missionList[index].StartMission();
+        }
+
+
         foreach (var checkPoint in _missionList)
         {
             checkPoint.CheckComplete();
         }
     }
 
-    public CheckPoint CurrentMission => _missionList.FirstOrDefault(item => !item.IsComplete);
+    
 }
